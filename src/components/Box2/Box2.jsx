273c24c1ac4aceb650649e75ea6987f9617330d2
Vikas Meshram <vikas.meshram@homznoffiz.com>
2023-06-04 16:43:31 +0530

    :bug: Completed Farmer Motion Learning.

---
+++ src/components/Box2/Box2.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
+const Box2 = () => {
+  const [isAnimating, setIsAnimating] = useState(false);
+  return (
+    <div className='box-container'>
+      <motion.div
+        className='box'
+        drag
+        dragConstraints={{
+          right: 20,
+          left: -20,
+          bottom: 5,
+        }}
+        whileHover={{
+          scale: 1.1,
+        }}
+        whileTap={{
+          scale: 0.9,
+        }}
+      ></motion.div>
+    </div>
+  );
+};
+
+export default Box2;
